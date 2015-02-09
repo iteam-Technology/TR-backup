fee32bc701b71a1851f79fc8e67e9e99805da888
iteam-Technology <iteam@iteamservices.com>
2015-02-08 20:39:33 -0500

    Update TR-backups.cpp

--- TR-backups.cpp
+++ TR-backups.cpp
@@ -1,10 +1,12 @@
+#include <iostream>
+
 int main() {
                
 double a,total,seconds,minutes;
 double MB = .005;
                
 while(a = true){
-  cin >> a;
+  std::cin >> a;
   seconds = (a / MB ); 
   minutes = seconds / 60;
   total = minutes / 60;
@@ -12,7 +14,7 @@ while(a = true){
     if(a <= 1)
       return 0;
                
- cout << a << " is original backup size " << " and it would take " << total << " hours to backup" << endl;
+ std::cout << a << " is original backup size " << " and it would take " << total << " hours to backup" << std::endl;
   }
  
 return 0;
